5e26938651dfac539c1c08ac04869762726130c3
davidrseves <davidrseves@gmail.com>
2023-03-06 13:34:39 +0100

    Server React
    Conexión comprobada mediante web-sockets

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebSocketExample from './WebSocketExample';
+
+function App() {
+  return (
+    <div className="App">
+      <WebSocketExample />
+    </div>
+  );
+}
+
+export default App;
