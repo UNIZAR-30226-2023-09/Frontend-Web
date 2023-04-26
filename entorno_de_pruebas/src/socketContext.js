71b16887889c05e47bf9f39b37a16da4d4db8232
davidrseves <davidrseves@gmail.com>
2023-04-27 00:20:59 +0200

    Entorno de pruebas react

---
+++ entorno_de_pruebas/src/socketContext.js
@@ -0,0 +1,31 @@
+/******************************************************************************\
+* Asignatura: Proyecto Software (2022/2023)                                    *
+* Fichero: socketContext.js                                                    *
+* Autor: David Rivera Seves (NIP: 815124)                                      *
+\******************************************************************************/
+
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const SocketContext = createContext();
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    //const newSocket = new WebSocket('ws://34.175.156.130:8080');
+    const newSocket = new WebSocket('ws://34.175.172.53:8080');
+    setSocket(newSocket);
+
+    return () => newSocket.close();
+  }, []);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
